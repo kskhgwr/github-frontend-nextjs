2c62879c7fc30a12e9f1a39a215e5f7420c87819
kskhgwr <kskhgwr@gmail.com>
2022-04-30 20:12:22 +0900

    testCode fix

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -2,7 +2,9 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Home from '../pages/index'
 
-it('Should render title text', () => {
+it('Should render title text', async () => {
   render(<Home />)
-  expect(screen.getByText('Next.js!')).toBeInTheDocument()
+  await (() => {
+    expect(screen.getByText('Next.js!')).toBeInTheDocument()
+  })
 })
